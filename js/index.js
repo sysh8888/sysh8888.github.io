284a083687c3af7ff9970c45c841982d9c99ff6b
sysh8888 <sysh8888@126.com>
2021-04-14 19:35:01 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var url1="https://bird.ioliu.cn/v1?url=https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0";
+var url1="https://bird.ioliu.cn/v1?url=https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=21&page_start=0";
 $(function(){
  $.ajax({
  async: true,
