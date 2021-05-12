0fc63044b0aae85d68d90741d1e55978da40bc86
sysh8888 <sysh8888@126.com>
2021-05-12 14:59:46 +0800

    Update zy.js

--- v/css/zy.js
+++ v/css/zy.js
@@ -1,10 +1,10 @@
 //内容开始
 var feifeiw={"0":"天空","2":"八戒"}; 
 var feifeiz={"0":"http://v8.tiankongzy.cc/inc/feifei3/","2":"http://cj.bajiecaiji.com/inc/feifei3bjm3u8/index.php"};
-var pingguow={"0":"天空","1":"快播","2":"萌果","3":"明日","4":"AWS","5":"被窝","6":"柚子","7":"影派","8":"嘀哩","9":"影派","10":"百度","11":"麻瓜"}; 
+var pingguow={"0":"天空","1":"快播","2":"萌果","3":"明日","4":"宅看","5":"被窝","6":"柚子","7":"三九","8":"嘀哩","9":"影派","10":"姨妈","11":"麻瓜"}; 
 var pingguoz={"0":"https://www.tiankongzy.cc/api.php/provide/vod/","1":"https://www.kuaibozy.com/api.php/provide/vod/",
               "2":"http://api.appearoo.top/api.php/provide/vod/","3":"http://zy.zcocc.com/api.php/provide/vod/",
-              "4":"http://www.awsltv.com/api.php/provide/vod/at/json/","5":"https://www.beiwoysw.com/api.php/provide/vod/",
-              "6":"http://tv.aitesucai.xyz/api.php/provide/vod/at/json/","7":"https://v.dtjos.com/api.php/provide/vod/at/json/",
+              "4":"https://www.zhaikanys.com/api.php/provide/vod/at/json/","5":"https://www.beiwoysw.com/api.php/provide/vod/",
+              "6":"http://tv.aitesucai.xyz/api.php/provide/vod/at/json/","7":"https://www.39kan.com/api.php/provide/vod/at/json/",
               "8":"http://diliktv.com/api.php/provide/vod/from/kbm3u8/at/json/","9":"https://v.dtjos.com/api.php/provide/vod/at/json/",
-              "10":"https://m3u8.apibdzy.com/api.php/provide/vod/at/json","11":"http://maguays.cc/api.php/provide/vod/at/json/"}; 
+              "10":"https://4k.4kyima.com/api.php/provide/vod/at/json/","11":"http://maguays.cc/api.php/provide/vod/at/json/"}; 
