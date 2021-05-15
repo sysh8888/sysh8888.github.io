fe01653179aa42452e37add4ff986ff9a8aa9621
sysh8888 <sysh8888@126.com>
2021-05-15 19:39:53 +0800

    Update zy.js

--- v/css/zy.js
+++ v/css/zy.js
@@ -3,10 +3,10 @@ var feifeiw={"0":"天空","2":"八戒"};
 var feifeiz={"0":"http://v8.tiankongzy.cc/inc/feifei3/","2":"http://cj.bajiecaiji.com/inc/feifei3bjm3u8/index.php"};
 var pingguow={"0":"天空","1":"快播","2":"木瓜","3":"零六","4":"奇遇","5":"被窝","6":"柚子","7":"三九","8":"嘀哩","9":"影派","10":"4K","11":"麻瓜"}; 
 var pingguoz={"0":"https://www.tiankongzy.cc/api.php/provide/vod/","1":"https://www.kuaibozy.com/api.php/provide/vod/",
-              "2":"https://www.gzfc.net/api.php/provide/vod/at/json/","3":"https://www.12306.in/api.php/provide/vod/at/json/",
-              "4":"https://www.newqiyu.com/api.php/provide/vod/at/json/","5":"https://www.beiwoysw.com/api.php/provide/vod/",
-              "6":"http://tv.aitesucai.xyz/api.php/provide/vod/at/json/","7":"https://www.39kan.com/api.php/provide/vod/at/json/",
-              "8":"http://diliktv.com/api.php/provide/vod/from/kbm3u8/at/json/","9":"https://v.dtjos.com/api.php/provide/vod/at/json/",
-              "10":"https://4k.4kyima.com/api.php/provide/vod/at/json/","11":"http://maguays.cc/api.php/provide/vod/at/json/"}; 
+              "2":"https://www.gzfc.net/api.php/provide/vod/","3":"https://www.12306.in/api.php/provide/vod/",
+              "4":"https://www.newqiyu.com/api.php/provide/vod/","5":"https://www.beiwoysw.com/api.php/provide/vod/",
+              "6":"http://tv.aitesucai.xyz/api.php/provide/vod/","7":"https://www.39kan.com/api.php/provide/vod/",
+              "8":"http://diliktv.com/api.php/provide/vod/from/kbm3u8/at/json/","9":"https://v.dtjos.com/api.php/provide/vod/",
+              "10":"https://4k.4kyima.com/api.php/provide/vod/","11":"http://maguays.cc/api.php/provide/vod/"}; 
 //萌果http://api.appearoo.top/api.php/provide/vod/,明日http://zy.zcocc.com/api.php/provide/vod/,奇遇https://www.newqiyu.com/api.php/provide/vod/at/json/
 //100iphttps://www.100ip.cn/api.php/provide/vod/at/json/,宅看https://www.zhaikanys.com/api.php/provide/vod/at/json/
