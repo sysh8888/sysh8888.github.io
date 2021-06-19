50ad2743912d1f08b6c9270ae78494c8095f91b5
sysh8888 <sysh8888@126.com>
2021-06-20 06:27:55 +0800

    Update zy.js

--- v/css/zy.js
+++ v/css/zy.js
@@ -2,7 +2,7 @@
 var feifeiw={"0":"天空","2":"八戒"}; 
 var feifeiz={"0":"http://v8.tiankongzy.cc/inc/feifei3/","2":"http://cj.bajiecaiji.com/inc/feifei3bjm3u8/index.php"};
 var pingguow={"0":"天空","1":"快播","2":"追剧","3":"一百","4":"唐朝","5":"人人","6":"柚子","7":"三九","8":"百度","9":"影派","10":"爱达","11":"麻瓜"}; 
-var pingguoz={"0":"https://api.tiankongapi.com/api.php/provide/vod/at/json/from/tkm3u8/","1":"https://www.kuaibozy.com/api.php/provide/vod/",
+var pingguoz={"0":"https://www.tiankongzy.cc/api.php/provide/vod/at/json/from/tkm3u8/","1":"https://www.kuaibozy.com/api.php/provide/vod/",
               "2":"https://www.huaxiab.com/api.php/provide/vod/at/json/","3":"https://www.100ip.cn/api.php/provide/vod/at/json/",
               "4":"https://tangchaozy.vip/api.php/provide/vod/","5":"https://www.rrzyw.cc/api.php/provide/vod/from/rrm3u8/at/json/",
               "6":"http://tv.aitesucai.xyz/api.php/provide/vod/","7":"https://www.39kan.com/api.php/provide/vod/",
