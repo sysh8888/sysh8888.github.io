522ad7242ec852985eaa3f373d2d6ddf38a372b7
sysh8888 <sysh8888@126.com>
2021-08-04 19:36:45 +0800

    Update index1.js

--- js/index1.js
+++ js/index1.js
@@ -16,6 +16,7 @@ console.log(data);
 var timu="";
 for (var i=0;i<20;i++)
 { timu+='<dl><dt><a target="_blank" href="./v/so.html?q='+data.subjects[i].title+'"><img src="'+data.subjects[i].cover+'"></a><span>豆瓣评分:'+data.subjects[i].rating.value+'分</span></dt><dd>'+data.subjects[i].title+'</dd></dl>';}
+  document.getElementById("lie").innerHTML =timu;
  }
  });
 })
