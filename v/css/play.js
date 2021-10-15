2467667ded183606060ce92858d56cd26f88172a
sysh8888 <sysh8888@126.com>
2021-10-15 13:11:16 +0800

    Update play.js

--- v/css/play.js
+++ v/css/play.js
@@ -7,7 +7,7 @@ $(function(){
  dataType: 'jsonp',
  jsonp: 'callback',
  jsonpCallback: 'callbackfunction',
- url: "https://bird.ioliu.cn/v1?url="+url1+lianjie,
+ url: jiekou+url1+lianjie,
  data: "",
  timeout: 3000,
  contentType: "application/json;utf-8",
