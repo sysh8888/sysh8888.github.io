d8c5af8c524c70baacebb13b34c3b55da60c0271
sysh8888 <sysh8888@126.com>
2021-10-17 11:16:20 +0800

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -15,7 +15,7 @@ function ying(ying){
   //console.log(data);
   var timu="";
   for (var i=0,l=data.data.ms.length;i<l;i++)
-  { timu+='<dl><dt><a target="_blank" href="./v/so.html?q='+data.data.ms[i].t+'&html=1.html"><img src="'+data.data.ms[i].img+'" class="img-responsive" alt="'+data.data.ms[i].t+'"></a><span>年份:'+data.data.ms[i].year+'</span></dt><dd>'+data.data.ms[i].t+'</dd></dl>';}
+  { timu+='<dl><dt><a target="_blank" href="./so.html?q='+data.data.ms[i].t+'&html=1.html"><img src="'+data.data.ms[i].img+'" class="img-responsive" alt="'+data.data.ms[i].t+'"></a><span>年份:'+data.data.ms[i].year+'</span></dt><dd>'+data.data.ms[i].t+'</dd></dl>';}
     document.getElementById("lie").innerHTML =timu;
    }
    });
