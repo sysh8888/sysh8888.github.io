bc7acaf53fadb6a3d3facd0359137cee07f17bfe
sysh8888 <sysh8888@126.com>
2021-10-24 16:55:55 +0800

    Create zy.js

--- v/css/zy.js
+++ v/css/zy.js
@@ -1,9 +1,9 @@
 //内容开始
 var feifeiw={"0":"天空","1":"八戒","2":"红牛"}; 
 var feifeiz={"0":"http://v8.tiankongzy.cc/inc/feifei3/","1":"http://cj.bajiecaiji.com/inc/feifei3bjm3u8/index.php","2":"https://www.hongniuzy.com/inc/feifei/"};
-var pingguow={"0":"天空","1":"剧好","2":"追剧","3":"无尽","4":"蘑菇","5":"畅视","6":"八零","7":"三九","8":"百度","9":"八八","10":"神马","11":"北斗","12":"影牛",
+var pingguow={"0":"思乐","1":"片库","2":"追剧","3":"无尽","4":"蘑菇","5":"畅视","6":"八零","7":"三九","8":"百度","9":"八八","10":"神马","11":"北斗","12":"影牛",
               "13":"瓜皮","14":"播猫","15":"飞鱼"}; 
-var pingguoz={"0":"https://www.tiankongzy.cc/api.php/provide/vod/at/json/from/tkm3u8/","1":"https://www.juhaokan.cc/api.php/provide/vod/at/json",
+var pingguoz={"0":"https://www.4ltv.com/api.php/provide/vod/","1":"https://pianku.wang/api.php/provide/vod/at/json/",
               "2":"http://www.huaxiab.com/api.php/provide/vod/","3":"https://www.wujinzy.com/api.php/provide/vod/from/wjm3u8/at/json/",
               "4":"http://www.moguzyw.com:520/moguzy.php/provide/vod/","5":"http://app.reboju.net/api.php/provide/vod/",
               "6":"http://zy.yilans.net:8090/api.php/provide/vod/at/json/","7":"https://www.39kan.com/api.php/provide/vod/",
@@ -13,9 +13,9 @@ var pingguoz={"0":"https://www.tiankongzy.cc/api.php/provide/vod/at/json/from/tk
               "14":"https://www.bofangya.com/api.php/provide/vod/at/json/","15":"https://app.feiyu5.com/api.php/provide/vod/at/json/"}; 
 //萌果http://api.appearoo.top/api.php/provide/vod/,明日,八零http://zy.yilans.net:8090/api.php/provide/vod/at/json/
 //蘑菇http://www.moguzyw.com:520/moguzy.php/provide/vod/，百度https://m3u8.apibdzy.com/api.php/provide/vod/at/json/
-//人人https://www.rrzyw.cc/api.php/provide/vod/from/rrm3u8/at/json/柚子http://tv.aitesucai.xyz/api.php/provide/vod/
+//天空https://www.tiankongzy.cc/api.php/provide/vod/at/json/from/tkm3u8/柚子http://tv.aitesucai.xyz/api.php/provide/vod/
 //35https://www.555dy6.com/api.php/provide/vod/at/json/安逸http://dy.51isu.com:11801/api.php/provide/vod/at/json/
-//唐朝http://beiyong.donggou.vip/api.php/provide/vod/快播https://www.kuaibozy.com/api.php/provide/vod/
+//剧好https://www.juhaokan.cc/api.php/provide/vod/at/json快播https://www.kuaibozy.com/api.php/provide/vod/
 //影派https://v.dtjos.com/api.php/provide/vod/一百https://www.100ip.cn/api.php/provide/vod/at/json/
 //速搜http://www.susouzy.com/api.php/provide/vod/,888http://ok.888hyk.com/api.php/provide/vod/at/json/
 //无尽https://api.wujinapi.com/api.php/provide/vod/from/wjm3u8/at/json/,TVBhttp://www.tvyb02.com/api.php/provide/vod/at/json
