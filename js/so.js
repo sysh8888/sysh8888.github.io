185db4247b6d1a2d17c656a47ac5343ef55f2fae
sysh8888 <sysh8888@126.com>
2021-10-28 13:53:48 +0800

    Update so.js

--- js/so.js
+++ js/so.js
@@ -16,9 +16,10 @@ $("#list").hide();
             var Node = $("#loading");  
                 Node.hide(); 
                 var jieguo="";
+				if(data.list.length==0){jieguo='<P style="font-size:15px;"><SPAN><b style="color:#CC0033" >暂无资源，请查看其他资源！</b></SPAN></P>'} else{
                 for (var i=0;i<data.list.length;i++)
                  { jieguo+=' <li class="am-g am-list-item-desced am-list-item-thumbed am-list-item-thumb-left"><div class="am-u-sm-4 am-list-thumb"><a href="./play.html?q='+data.list[i].vod_id+'&id='+id+'&zy=pg" target="_blank" class="search-item-href"><img src="'+data.list[i].vod_pic+'"  alt="'+data.list[i].vod_name+'"></a></div><span class="video-score" title="国家">'+data.list[i].vod_remarks+'</span><div class="am-u-sm-8 am-list-main"><h3 class="am-list-item-hd"><a href="./play.html?q='+data.list[i].vod_id+'&id='+id+'&zy=pg" target="_blank" class="search-item-href">'+data.list[i].vod_name+'</a></h3><div class="am-list-item-text">'+data.list[i].vod_blurb+'</div><p class="am-list-item-text"><b>主演:</b><span>'+data.list[i].vod_actor+'</span></p><a href="./play.html?q='+data.list[i].vod_id+'&id='+id+'&zy=pg" target="_blank" class="am-btn am-btn-secondary am-btn-sm search-item-btn"><i class="am-icon-play"></i>在线播放  </a></div></li> ';}
-                          
+			 } 
               $("#list").html(jieguo);
                $("#list").slideDown();
             }else{
@@ -82,16 +83,15 @@ function qitaziyuan(id){
   success: function(data)  {
   //console.log(data);
   var timu1="";
-  
-  for (var i=0,l=data.data.length;i<l;i++){
-
-    
+  if(data.data.length==0){timu1='<P style="font-size:15px;"><SPAN><b style="color:#CC0033" >暂无资源，请查看其他资源！</b></SPAN></P>'} else{
+  for (var i=0,l=data.data.length;i<l;i++){ 
  timu1 +='<li class="am-g am-list-item-desced am-list-item-thumbed am-list-item-thumb-left"><div class="am-u-sm-4 am-list-thumb"><a href="./play.html?q='+data.data[i].vid+'&id='+id+'&zy=qt" target="_blank" class="search-item-href"><img src="'+data.data[i].vod_pic+'"  alt="'+data.data[i].vod_name+'" ></a></div><span class="video-score" title="国家">'+data.data[i].vod_remarks+'</span><div class="am-u-sm-8 am-list-main"><h3 class="am-list-item-hd"><a href="./play.html?q='+data.data[i].vid+'&id='+id+'&zy=qt" target="_blank" class="search-item-href">'+data.data[i].vod_name+'</a></h3><div class="am-list-item-text">'+data.data[i].vod_blurb+'</div><p class="am-list-item-text"><b>主演:</b><span>'+data.data[i].vod_actor+'</span></p><a href="./play.html?q='+data.data[i].vid+'&id='+id+'&zy=qt" target="_blank" class="am-btn am-btn-secondary am-btn-sm search-item-btn"><i class="am-icon-play"></i>在线播放  </a></div></li>';}
- document.getElementById("qtlist").innerHTML =timu1;
-    }
-  });
+
+  }
+   document.getElementById("qtlist").innerHTML =timu1;
+  }
  })
- }
+})}
  //苹果接口番茄资源
 function zhanwai(id){
 var url5="http://api.fqzy.cc/api.php/provide/vod/at/json/";
@@ -103,8 +103,11 @@ var url5="http://api.fqzy.cc/api.php/provide/vod/at/json/";
            success: function(data) {
     
                 var jieguo1="";
+				  if(data.list.length==0){jieguo1='<P style="font-size:15px;"><SPAN><b style="color:#CC0033" >暂无资源，请查看其他资源！</b></SPAN></P>'} else{
+				
                 for (var i=0;i<data.list.length;i++)
                  { jieguo1+=' <li class="am-g am-list-item-desced am-list-item-thumbed am-list-item-thumb-left"><div class="am-u-sm-4 am-list-thumb"><a href="./play.html?q='+data.list[i].vod_id+'&id='+id+'&zy=fq" target="_blank" class="search-item-href"><img src="'+data.list[i].vod_pic+'"  alt="'+data.list[i].vod_name+'"></a></div><span class="video-score" title="国家">'+data.list[i].vod_remarks+'</span><div class="am-u-sm-8 am-list-main"><h3 class="am-list-item-hd"><a href="./play.html?q='+data.list[i].vod_id+'&id='+id+'&zy=fq" target="_blank" class="search-item-href">'+data.list[i].vod_name+'</a></h3><div class="am-list-item-text">'+data.list[i].vod_blurb+'</div><p class="am-list-item-text"><b>主演:</b><span>'+data.list[i].vod_actor+'</span></p><a href="./play.html?q='+data.list[i].vod_id+'&id='+id+'&zy=fq" target="_blank" class="am-btn am-btn-secondary am-btn-sm search-item-btn"><i class="am-icon-play"></i>在线播放  </a></div></li> ';}
+				  }
                 document.getElementById("zwlist").innerHTML =jieguo1;           
            
            
