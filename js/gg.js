8be8814d36af831841c7e8efe1885346e7544d3b
sysh8888 <sysh8888@126.com>
2021-11-12 13:45:20 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,4 +1,5 @@
 var jiekou="http://so.7080.wang/index.php?url=";
 var jiekou1="https://bird.ioliu.cn/v1?url=";
 var jiekou2="http://7080.eu5.org/json/index.php?url=";
+var jiekou3="http://b.7080.wang:666/v1/?url=";
 var toutou='<li><a href="//7080.wang">首页</a></li><li><a href="/v/">影视搜</a></li><li><a href="/live/">电视直播</a></li>';
