5a4b45c5e641bd8ca688997a51dc0ff6c57f2280
sysh8888 <sysh8888@126.com>
2021-11-12 13:16:02 +0800

    Update fqplay.js

--- js/fqplay.js
+++ js/fqplay.js
@@ -1,18 +1,23 @@
 //内容开始
-url1=pingguoz[id];
+var url1=fqzy[id];
+var playy=fqplay[id];
 $(function(){
  $.ajax({
  async: true,
  type: "GET",
- dataType: 'jsonp',
  jsonp: 'callback',
- jsonpCallback: 'callbackfunction',
- url: jiekou +"http://api.fqzy.cc/api.php/provide/vod/at/json/?ac=detail&ids="+lianjie,
- data: "",
- timeout: 3000,
- contentType: "application/json;utf-8",
+ jsonpCallback: 'callback',
+ url: jiekou +url1+"?ac=detail&ids="+lianjie,
+  data: "",
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+                 alert(textStatus);
+                 alert(errorThrown);
+                 this; // 调用本次AJAX请求时传递的options参数
+             },
+             dataType: "jsonp",
+			 contentType: "application/json;utf-8",
  success: function(data) {
-console.log(data);
+//console.log(data);
 var bolie=new Array();
 bolie=data.list["0"].vod_play_from.split("$$$");
 var bolie1="";
@@ -30,17 +35,15 @@ for (h=0;h<strs.length ;h++){
 bolie1 +='<li class="'+boliee+'" ><a href="#'+bolie[h]+'">'+bolie[h]+'</a></li>';
   for (i=0;i<str1[h].length ;i+=2)
   {
-      neirong1 +='<li class="am-btn am-btn-sm btn-play-source"><a href="https://jiexi.8b5q.cn/player/jx.php?url='+str1[h][i+1]+'"  target="ajax" onclick="GetHref(this);return false;">'+str1[h][i]+'</a>';
+      neirong1 +='<li class="am-btn am-btn-sm btn-play-source"><a href="'+playy+str1[h][i+1]+'"  target="ajax" onclick="GetHref(this);return false;">'+str1[h][i]+'</a>';
   }
 neirong +='<div class="'+neinei+'" id="'+bolie[h]+'">'+neirong1+'</div>';
 var neirong1="";
 }
 //console.log(str1);
-var vide1=str1[0][0]; 
 var vide=str1[0][1];  
-document.getElementById("demo1").innerHTML =vide1;
 document.getElementById("bolie2").innerHTML =bolie1;
-document.getElementById("video").src="https://jiexi.8b5q.cn/player/jx.php?url="+vide ;
+document.getElementById("video").src=playy+vide ;
 document.getElementById("neirong").innerHTML =neirong;
 document.getElementById("name").innerHTML =data.list[0].vod_name;
 document.getElementById("tu").src=data.list[0].vod_pic;
