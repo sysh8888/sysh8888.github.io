4417c44bc950c257115cc3116f881410fc3998d3
sysh8888 <sysh8888@126.com>
2021-11-13 19:10:53 +0800

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -1,5 +1,5 @@
 function ying(ying){
-  var url1=jiekou+"http://front-gateway.mtime.com/ticket/schedule/showing/movies.api?locationId="+ying;
+  var url1=jiekou3+"http://front-gateway.mtime.com/ticket/schedule/showing/movies.api?locationId="+ying;
   $(function(){
    $.ajax({
    async: true,
