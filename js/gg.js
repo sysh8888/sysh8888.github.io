7f2eba1dcd9608c64c415785e0e9198f50b0e731
sysh8888 <sysh8888@126.com>
2021-11-16 15:58:39 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,7 +1,7 @@
 var jiekou="https://7080.c.freefrp.net/v1/?url=";
 var jiekou1="https://bird.ioliu.cn/v1?url=";
-var jiekou2="http://7080.eu5.org/json/index.php?url=";
+var jiekou2="http://7080.eu5.org/json/1.php?url=";
 var jiekou3="https://7080.c.freefrp.net/v1/?url=";
-var jiekou4="http://host084219.free.cnaaa7.com/json/?url=";
-var jiekou5="http://so.7080.wang/index.php?url=";
+var jiekou4="http://host084219.free.cnaaa7.com/json/1.php?url=";
+var jiekou5="http://so.7080.wang/1.php?url=";
 var toutou='<li><a href="//7080.wang">首页</a></li><li><a href="/v/">影视搜</a></li><li><a href="/live/">电视直播</a></li>';
