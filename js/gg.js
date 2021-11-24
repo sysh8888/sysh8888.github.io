8b6c23a930dc50db34c5b9a3f7807ab5cef3c533
sysh8888 <sysh8888@126.com>
2021-11-24 13:20:02 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,9 +1,10 @@
-var jiekou="http://b.7080.wang:666/v1/?url=";
-var jiekou1="https://bird.ioliu.cn/v1?url=";
-var jiekou2="http://7080.eu5.org/json/huancun.php?url=";
+var jiekou="http://b.7080.wang:666/v1/?url=";//通用
+var jiekou1="https://bird.ioliu.cn/v1?url=";//无法使用
+var jiekou2="http://7080.unaux.com/json/1.php?url=";//高清
 var jiekou3="https://7080.c.freefrp.net/v1/?url=";
 var jiekou4="http://host084219.free.cnaaa7.com/json/cache/demo/huancun.php?url=";
 var jiekou5="http://so.7080.wang/index.php?url=";
 var jiekou6="http://7080w.vaiwan.com/json/huancun.php?url=";
 var jiekou7="http://7080.eu5.org/json/1.php?url=";
+var jiekou8="http://7080.unaux.com/json/1.php?url=";
 var toutou='<li><a href="//7080.wang">首页</a></li><li><a href="/v/">影视搜</a></li><li><a href="/live/">电视直播</a></li>';
