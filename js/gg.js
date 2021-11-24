4905147dac139af5f40ccea91e8512671790a041
sysh8888 <sysh8888@126.com>
2021-11-24 13:34:18 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,6 +1,6 @@
 var jiekou="http://b.7080.wang:666/v1/?url=";//通用
 var jiekou1="https://bird.ioliu.cn/v1?url=";//无法使用
-var jiekou2="http://7080.unaux.com/json/1.php?url=";//高清
+var jiekou2="http://7080.eu5.org/json/huancun.php?url=";//高清
 var jiekou3="https://7080.c.freefrp.net/v1/?url=";
 var jiekou4="http://host084219.free.cnaaa7.com/json/cache/demo/huancun.php?url=";
 var jiekou5="http://so.7080.wang/index.php?url=";
