a96176525c171bde255c4f1f7bb47ffbbcaee634
sysh8888 <sysh8888@126.com>
2021-11-26 10:25:36 +0800

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -1,5 +1,5 @@
 function ying(ying){
-  var url1=jiekou7+"http://front-gateway.mtime.com/ticket/schedule/showing/movies.api?locationId="+ying;
+  var url1=jiekou4+"http://front-gateway.mtime.com/ticket/schedule/showing/movies.api?locationId="+ying;
   $(function(){
    $.ajax({
    async: true,
