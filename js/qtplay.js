56a46a5a23a5e1d8a14a629f05d5f6c77d95a303
sysh8888 <sysh8888@126.com>
2022-02-16 14:38:35 +0800

    Add files via upload

--- js/qtplay.js
+++ js/qtplay.js
@@ -1,7 +1,58 @@
 
 //内容开始
 url4=appzy[id]["url"]+"/detail?vod_id=";
-$(function(){
+$(function (){
+var dataroot=url4+lianjie;
+$.getJSON(dataroot, function(data){
+	 var bolie=new Array();
+  bolie=data.data.vod_play_from.split("$$$");
+  var bolie1="";
+  var neirong1='';
+  var neirong='';
+  var dianbo=data.data.vod_play_url;
+  var dianbo1=dianbo.replace(/\r\n/g,"#") ;
+  var dianbo2=dianbo1.replace(/\$/g,"#") ;
+  var strs= new Array();
+  strs=dianbo2.split("###");
+ console.log(strs);
+  var str1= new Array();
+  for (h=0;h<strs.length ;h++){
+    var strr=strs[h].replace(/\#\#/g,"#") ;
+    str1[h]=strr.split("#");
+    if(h==0){var neinei="am-tab-panel am-fade am-in am-active";var boliee='am-active';}else{var neinei="am-tab-panel am-fade tv-res"; var boliee='am';}
+  bolie1 +='<li class="'+boliee+'" ><a href="#'+bolie[h]+'">'+bolie[h]+'</a></li>';
+    for (i=0;i<str1[h].length ;i+=2)
+    {
+        neirong1 +='<li class="am-btn am-btn-sm btn-play-source"><a href="./jx/index.html?url='+str1[h][i+1]+'"  target="ajax" onclick="GetHref(this);return false;">'+str1[h][i]+'</a></li>';
+    }
+  neirong +='<div class="'+neinei+'" id="'+bolie[h]+'">'+neirong1+'</div>';
+  var neirong1="";
+  }
+  //console.log(str1);
+  var vide1=str1[0][0];
+  var vide=str1[0][1];  
+  document.getElementById("demo1").innerHTML=vide1;
+  document.getElementById("video").src="./jx/index.html?url="+vide ;
+  document.getElementById("bolie2").innerHTML =bolie1;
+  document.getElementById("neirong").innerHTML =neirong;
+  document.getElementById("name").innerHTML =data.data.vod_name;
+  document.getElementById("tu").src=data.data.vod_pic;
+  document.getElementById("nian").innerHTML =data.data.vod_year;
+  document.getElementById("diqu").innerHTML =data.data.vod_area;
+  document.getElementById("daoyan").innerHTML =data.data.vod_director;
+  document.getElementById("fenlei").innerText=data.data.vod_class;
+  document.getElementById("yanyuan").innerText=data.data.vod_actor;
+  document.getElementById("jiesao").innerText=data.data.vod_blurb;
+  document.getElementById("jiesao1").innerText=data.data.vod_content;
+	
+});
+
+})
+
+
+
+//备用
+function zy(){
  $.ajax({
  async: true,
  type: "GET",
@@ -14,7 +65,7 @@ $(function(){
  timeout: 8000,
  contentType: "application/json;utf-8",
  success: function(data) {
-  console.log(data);
+ // console.log(data);
   var bolie=new Array();
   bolie=data.data.vod_play_from.split("$$$");
   var bolie1="";
@@ -57,4 +108,4 @@ $(function(){
   document.getElementById("jiesao1").innerText=data.data.vod_content;
    }
    });
-  })
+  }
--- js/so.js
+++ js/so.js
@@ -41,6 +41,30 @@ function frompg(id){
   
 //其他资源接口APP
 function zy2(id){
+		$("#list2").hide();
+  var url3=appzy[id]["url"]+"?page=1&wd="+ lianjie1;
+var dataroot=url3;
+$.getJSON(dataroot, function(data){
+       var Node = $("#loading");  
+                  Node.hide(); 
+    var jieguo2="";
+    if(data.data.list.length==0){jieguo2='<P style="font-size:15px;"><SPAN><b style="color:#CC0033" >暂无资源，请查看其他资源！</b></SPAN></P>'} else{
+  
+          for (var i=0;i<data.data.list.length;i++)
+			       { jieguo2+='<li><a class="movie-item" href="./play.html?q='+data.data.list[i].vod_id+'&id='+id+'&zy=qt" target="_blank"><div class="movie-cover"><img src="'+data.data.list[i].vod_pic+'" ><span class="movie-description"><i class="description-bg"></i><p>状态：'+data.data.list[i].vod_remarks+'</p><p>年代：'+data.data.list[i].vod_year+'</p><p>&gt; 在线观看</p></span></div><div class="movie-title"><p class="movie-name">'+data.data.list[i].vod_name+'</p><p class="movie-tags" title="演员">'+data.data.list[i].vod_actor+'</p></div></a></li>';} 
+
+  }
+    $("#list2").html(jieguo2);
+                 $("#list2").slideDown();
+
+});
+document.getElementById("bolei2").innerHTML =appzy[id]["name"]; 
+}
+
+
+
+//APP接口备用
+function zy21(id){
 	$("#list2").hide();
   var url3=appqt +appzy[id]["url"]+"?page=1&wd="+ lianjie1;
   $(function (){
