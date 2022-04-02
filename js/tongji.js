3215f3ea7d5e6f040dd9ebc47ee9502e69f516b3
sysh8888 <sysh8888@126.com>
2022-04-02 13:43:11 +0800

    Create tongji.js

---
+++ js/tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6b933b9e111e81dd530fcaddb9159145";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
