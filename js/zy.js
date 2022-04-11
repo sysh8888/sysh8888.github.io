e03a5f165f257b4f79fb8e26fc7173f723eee462
sysh8888 <sysh8888@126.com>
2022-04-11 12:13:49 +0800

    Update zy.js

--- js/zy.js
+++ js/zy.js
@@ -17,15 +17,15 @@ var pingguozy={"0":{"name":"番茄","url":"https://api.fqzy.cc/api.php/provide/v
                "15":{"name":"CK","url":"https://ckzy.me/api.php/provide/vod/","play":"./jx/index.html?url="}};
 //其他APP
 var appzy={"0":{"name":"300看世界","url":"https://300ys.xyz/api.php/v1.vod"},
-           "1":{"name":"大熊","url":"https://qnys5566.com/mogai_api.php/v1.vod"},
+           "1":{"name":"月儿影视","url":"https://yue52.xyz/api.php/v1.vod"},
            "2":{"name":"爱看美剧","url":"https://www.uumjw.com/api.php/v1.vod"},
            "3":{"name":"爱酷影视","url":"https://www.zhanlangbu.com/ruifenglb_api.php/v1.vod"},
            "4":{"name":"冷视","url":"https://len.tv/api.php/v1.vod"},
-           "5":{"name":"奇趣影视","url":"https://app.qiqu.me/mogai_api.php/v1.vod"},
-           "6":{"name":"叮当影视","url":"https://tv.0573wl.com/api.php/v1.vod"},
+           "5":{"name":"U5影视","url":"https://appx.uy07.com/api.php/v1.vod"},
+           "6":{"name":"爱影视","url":"https://www.hzlff.cn/api.php/v1.vod"},
            "7":{"name":"初心影视","url":"https://www.18mv.club/api.php/v1.vod"},
            "8":{"name":"QC影视","url":"https://www.qcsvip.com/mogai_api.php/v1.vod"},
-           "9":{"name":"手指影视","url":"https://szys5678.com/mogai_api.php/v1.vod"},
+           "9":{"name":"看看剧","url":"https://www.kankanju.cn/api.php/v1.vod"},
            "10":{"name":"虎猫视频","url":"https://humaosp.com/mogai_api.php/v1.vod"},
            "11":{"name":"美剧虫","url":"https://meijuchong.com/api.php/v1.vod"}};	
 
