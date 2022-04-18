c508c6b295b590ae1eff88230cb4b53ae4f03baf
sysh8888 <sysh8888@126.com>
2022-04-18 12:14:08 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -8,7 +8,7 @@ var jiekou6="http://7080w.vaiwan.com/json/huancun.php?url=";
 var jiekou7="http://7080.eu5.org/json/2.php?url=";
 var jiekou8="http://7080.unaux.com/json/1.php?url=";
 var jiekou9="http://7080w.vaiwan.com/json/huancun.php?url=";
-var jsonjx="https://www.5020.work/json/jx.php?url=";//通用
+var jsonjx="https://www.5020.work/json/jx1.php?url=";//通用
 var appqt=jiekou4;
 var appfq=jiekou5;
 var toutou='<li><a href="//7080.wang">首页</a></li><li><a href="../360.html?catid=1&rank=rankhot&cat=&year=&area=&act=&pageno=1">电影</a></li><li><a href="../360.html?catid=2&rank=rankhot&cat=&year=&area=&act=&pageno=1">电视剧</a></li><li><a href="../360.html?catid=3&rank=rankhot&cat=&year=&area=&act=&pageno=1">综艺</a></li><li><a href="../360.html?catid=3&rank=rankhot&cat=&year=&area=&act=&pageno=1">动漫</a></li><li><a href="/app.html?zy=0&type=0&class=&area=&year=&by=&limit=21&page=1">APP资源</a></li><li><a href="/live/index.html">IPTV</a></li><li><a href="/dh/index.html">导航页</a></li><li class="am-dropdown" data-am-dropdown><a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:;">其他<span class="am-icon-caret-down"></span></a><ul class="am-dropdown-content"><li><a href="http://sysh8888.xiaopangkj.space/" target="_blank">52影视</a></li><li><a href="http://7080.unaux.com/" target="_blank">九亿影视</a></li></ul></li>';
