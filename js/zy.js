fad21abf8461a29af3d9e4ff6f3ca7c70f0a5a7c
sysh8888 <sysh8888@126.com>
2022-12-16 19:31:22 +0800

    Update zy.js

--- js/zy.js
+++ js/zy.js
@@ -17,13 +17,12 @@ var pingguozy={"0":{"name":"三九","url":"https://www.39kan.com/api.php/provide
                "15":{"name":"松鼠","url":"https://m3u8.songshuzy.com/api.php/provide/vod/","play":"./jx/index.html?url="}};
 //其他APP
 var appzy={   
-           "0":{"name":"我爱跟剧","url":"https://www.genmov.com/mogai_api.php/v1.vod"},
-           "1":{"name":"爱酷影视","url":"https://www.zhanlangbu.com/ruifenglb_api.php/v1.vod"},
-           "2":{"name":"淘剧社","url":"https://app.shuhai99.com/api.php/v1.vod"},
-           "3":{"name":"暖光影视","url":"https://app.bl210.com/api.php/v1.vod"},
-           "4":{"name":"龙猫影视","url":"https://lb.ybnxcx.com/api.php/v1.vod"},
-           "5":{"name":"佳佳影视","url":"https://www.jiajia.pub/api.php/v1.vod"},
-           "6":{"name":"看剧吧","url":"https://app.ishen520.com/api.php/v1.vod"}};		
+           "0":{"name":"爱酷影视","url":"https://www.zhanlangbu.com/ruifenglb_api.php/v1.vod"},
+           "1":{"name":"淘剧社","url":"https://app.shuhai99.com/api.php/v1.vod"},
+           "2":{"name":"暖光影视","url":"https://app.bl210.com/api.php/v1.vod"},
+           "3":{"name":"龙猫影视","url":"https://lb.ybnxcx.com/api.php/v1.vod"},
+           "4":{"name":"佳佳影视","url":"https://www.jiajia.pub/api.php/v1.vod"},
+           "5":{"name":"看剧吧","url":"https://app.ishen520.com/api.php/v1.vod"}};		
 
 //其他番茄、优剧、欧乐、1080tv
 var fqzy={"0":{"name":"番茄","url":"http://api.fqzy.cc/api.php/provide/vod/at/json/","play":"https://jiexi.8b5q.cn/player/jx.php?url="},
