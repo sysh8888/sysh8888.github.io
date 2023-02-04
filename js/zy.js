3032fde21bdc268c6c69e8141ec863797c048d59
sysh8888 <sysh8888@126.com>
2023-02-04 14:35:46 +0800

    Update zy.js

--- js/zy.js
+++ js/zy.js
@@ -17,11 +17,11 @@ var pingguozy={"0":{"name":"三九","url":"https://www.39kan.com/api.php/provide
                "15":{"name":"松鼠","url":"https://m3u8.songshuzy.com/api.php/provide/vod/","play":"./jx/index.html?url="}};
 //其他APP
 var appzy={   
-           "0":{"name":"爱酷影视","url":"https://www.zhanlangbu.com/ruifenglb_api.php/v1.vod"},
+           "0":{"name":"188看","url":"https://188kan.com/api.php/v1.vod"},
            "1":{"name":"淘剧社","url":"https://app.shuhai99.com/api.php/v1.vod"},
            "2":{"name":"暖光影视","url":"https://app.bl210.com/api.php/v1.vod"},
            "3":{"name":"龙猫影视","url":"https://lb.ybnxcx.com/api.php/v1.vod"},
-           "4":{"name":"佳佳影视","url":"https://www.jiajia.pub/api.php/v1.vod"},
+	   "4":{"name":"低端影视","url":"https://www.dddy.me/api.php/v1.vod"},
            "5":{"name":"看剧吧","url":"https://app.ishen520.com/api.php/v1.vod"}};		
 
 //其他番茄、优剧、欧乐、1080tv
