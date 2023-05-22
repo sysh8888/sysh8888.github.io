846c3b738a94bf30e31a12b202cd7e057a4262f6
sysh8888 <sysh8888@126.com>
2023-05-22 21:38:31 +0800

    Create zy1.js

---
+++ js/zy1.js
@@ -0,0 +1,5 @@
+//其他APP
+var appzy=[{"name":"123看","url":"https://123aktv.com/api.php/v1.vod"},
+           {"name":"霹雳影视","url":"https://plyy.cc/api.php/v1.vod"},
+          {"name":"188看","url":"https://www.188kan.com/api.php/v1.vod"},
+           {"name":"免费影视","url":"https://wlgwys.com/api.php/v1.vod"}];	
