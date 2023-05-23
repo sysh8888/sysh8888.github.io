f6e1028dbe8f6c5138f3eac37a3b26ca0f32208a
sysh8888 <sysh8888@126.com>
2023-05-23 13:33:32 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,9 +1,15 @@
-var jiekou="https://www.5020.work/json/?url=";
-var jiekou1="https://bird.ioliu.cn/v1?url=";
-var jiekou2="http://host084219.free.cnaaa7.com/json/huancun.php?url=";
-var jiekou3="http://so.7080.wang/json/huancun.php?url=";
-var jsonjx="https://www.5020.work/json/jx1.php?url=";
-var appqt=jiekou1;
-var appfq=jiekou2;
-var toutou='<li><a href="//7080.wang">首页</a></li><li><a href="../360.html?catid=1&rank=rankhot&cat=&year=&area=&act=&pageno=1">电影</a></li><li><a href="../360.html?catid=2&rank=rankhot&cat=&year=&area=&act=&pageno=1">电视剧</a></li><li><a href="../360.html?catid=3&rank=rankhot&cat=&year=&area=&act=&pageno=1">综艺</a></li><li><a href="../360.html?catid=3&rank=rankhot&cat=&year=&area=&act=&pageno=1">动漫</a></li><li><a href="/app.html?zy=0&type=0&class=&area=&year=&by=&limit=21&page=1">APP资源</a></li><li><a href="/live/index.html">IPTV</a></li><li><a href="http://mao.7080.wang" target="_blank">APP下载</a></li><li class="am-dropdown" data-am-dropdown><a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:;">其他<span class="am-icon-caret-down"></span></a><ul class="am-dropdown-content"><li><a href="http://so.7080.wang/" target="_blank">影视搜</a></li><li><a href="/if/index.html?dz=https://www.ikanbot.com/" target="_blank">爱看机器人</a></li></ul></li>';
-var souji='<li><a href="//7080.wang" class=""><img src="/img/film.png" alt="首页 " /><span class="am-navbar-label">首页</span></a></li><li><a href="/360.html?catid=1&rank=rankhot&cat=&year=&area=&act=&pageno=1" class=""><img src="/img/film.png" alt="电影" /><span class="am-navbar-label">电影</span></a></li><li><a href="/360.html?catid=2&rank=rankhot&cat=&year=&area=&act=&pageno=1" class=""><img src="/img/mg.png" alt="电视剧 " /><span class="am-navbar-label">电视剧</span></a></li><li><a href="/app.html?zy=0&type=0&class=&area=&year=&by=&limit=21&page=1" class=""><img src="/img/2mm.png" alt="APP资源" /><span class="am-navbar-label">APP资源</span></a></li><li><a href="/live/index.html" class=""><img src="/img/zongyi.png" alt="IPTV" /><span class="am-navbar-label">直播</span></a></li>';
+var jiekou="https://www.5020.work/json/?url=";//通用
+var jiekou1="http://b.7080.wang:666/v1/?url=";//无法使用https://bird.ioliu.cn/v1?url=
+var jiekou2="http://7080.eu5.org/json/huancun.php?url=";//高清
+var jiekou3="https://7080.c.freefrp.net/v1/?url=";
+var jiekou4="http://host084219.free.cnaaa7.com/json/huancun.php?url=";
+var jiekou5="http://so.7080.wang/json/huancun.php?url=";
+var jiekou6="http://7080w.vaiwan.com/json/huancun.php?url=";
+var jiekou7="http://7080.eu5.org/json/2.php?url=";
+var jiekou8="http://7080.unaux.com/json/1.php?url=";
+var jiekou9="http://7080w.vaiwan.com/json/huancun.php?url=";
+var jsonjx="https://www.5020.work/json/jx1.php?url=";//通用
+var appqt=jiekou4;
+var appfq=jiekou5;
+var toutou='<li><a href="//7080.wang">首页</a></li><li><a href="/app.html?zy=0&type=0&class=&area=&year=&by=&limit=21&page=1">APP分类</a></li><li><a href="/live/index.html">IPTV</a></li><li><a href="https://mao.7080.wang" target="_blank">APP下载</a></li>';
+var souji='<li><a href="//7080.wang" class=""><img src="/img/film.png" alt="首页 " /><span class="am-navbar-label">首页</span></a></li><span class="am-navbar-label">APP分类</span></a></li><li><a href="/live/index.html" class=""><img src="./img/zongyi.png" alt="IPTV" /><span class="am-navbar-label">电视直播</span></a></li>';
