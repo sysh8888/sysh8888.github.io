6ff0eff7976e87f7a692d9211e1ba9a70edd7373
sysh8888 <sysh8888@126.com>
2023-05-23 13:16:55 +0800

    Update zy1.js

--- js/zy1.js
+++ js/zy1.js
@@ -2,4 +2,7 @@
 var appzy=[{"name":"123看","url":"https://123aktv.com/api.php/v1.vod"},
            {"name":"霹雳影视","url":"https://plyy.cc/api.php/v1.vod"},
           {"name":"188看","url":"https://www.188kan.com/api.php/v1.vod"},
-           {"name":"免费影视","url":"https://wlgwys.com/api.php/v1.vod"}];	
+           {"name":"天天影视","url":"https://app.qianju.cc/api.php/v1.vod"},
+		   {"name":"汪了个汪","url":"https://wlgwys.com/api.php/v1.vod"},
+		   {"name":"海玉","url":"https://tv2.hykjtv.cn/api.php/v1.vod"}
+		   ];	
