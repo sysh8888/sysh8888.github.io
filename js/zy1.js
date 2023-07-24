2bdcdb55f5e3da1031c5e90e118c8ebcaa2d01eb
sysh8888 <sysh8888@126.com>
2023-07-24 13:50:07 +0800

    Update zy1.js

--- js/zy1.js
+++ js/zy1.js
@@ -4,7 +4,7 @@ var appzy=[{"name":"创意","url":"https://www.30dian.cn/api.php/v1.vod"},
            {"name":"霹雳影视","url":"https://plyy.cc/api.php/v1.vod"},
 	{"name":"真爱","url":"https://www.chiguafilm.com/api.php/v1.vod"},
            {"name":"天天影视","url":"https://app.qianju.cc/api.php/v1.vod"},
-	    {"name":"HG","url":"https://hong.hgyx.vip/api.php/v1.vod"},
+	    {"name":"龙门","url":"https://movie.iqinu.com/api.php/v1.vod"},
 	   {"name":"何以","url":"https://www.heyik.cn/api.php/v1.vod"},
 	 {"name":"木白","url":"https://www.chiguafilm.com/api.php/v1.vod"},
 		    {"name":"光年","url":"https://www.gntv.live/api.php/v1.vod"}
