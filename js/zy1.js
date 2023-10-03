db53324efd641c6d7439159ef96ed47302c3d8bd
sysh8888 <sysh8888@126.com>
2023-10-03 17:03:19 +0800

    Update zy1.js

--- js/zy1.js
+++ js/zy1.js
@@ -1,13 +1,8 @@
 //其他APP
-var appzy=[{"name":"创意","url":"https://www.30dian.cn/api.php/v1.vod"},
-           {"name":"看剧吧","url":"https://app.ishen520.com/api.php/v1.vod"},
+var appzy=[
           {"name":"全民","url":"https://www.qmystv.com/api.php/v1.vod"}, 
-	{"name":"真爱","url":"https://www.chiguafilm.com/api.php/v1.vod"},
            {"name":"天天影视","url":"https://app.qianju.cc/api.php/v1.vod"},
 	    {"name":"龙门","url":"https://movie.iqinu.com/api.php/v1.vod"},
 	   {"name":"何以","url":"https://www.heyik.cn/api.php/v1.vod"},
-	 {"name":"木白","url":"https://www.chiguafilm.com/api.php/v1.vod"},
-		    {"name":"光年","url":"https://www.gntv.live/api.php/v1.vod"},
-	  {"name":"聚焦","url":"https://www.mjlnl.cn/api.php/v1.vod"},
-	   {"name":"吃瓜","url":"https://www.chiguafilm.com/api.php/v1.vod"}
+	  {"name":"聚焦","url":"https://www.mjlnl.cn/api.php/v1.vod"}
 		   ];	
