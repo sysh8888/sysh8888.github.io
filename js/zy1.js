00451c29daebb5efad5a95caffe97221bf4ad120
sysh8888 <sysh8888@126.com>
2023-10-21 19:55:58 +0800

    Update zy1.js

--- js/zy1.js
+++ js/zy1.js
@@ -1,8 +1,12 @@
 //其他APP
 var appzy=[
-          {"name":"全民","url":"https://www.qmystv.com/api.php/v1.vod"}, 
-           {"name":"天天影视","url":"https://app.qianju.cc/api.php/v1.vod"},
-	    {"name":"龙门","url":"https://movie.iqinu.com/api.php/v1.vod"},
-	   {"name":"何以","url":"https://www.heyik.cn/api.php/v1.vod"},
-	  {"name":"聚焦","url":"https://www.mjlnl.cn/api.php/v1.vod"}
-		   ];	
+    {"name":"爱城","url":"https://90hd.top/api.php/v1.vod"}, 
+    {"name":"奇乐","url":"https://qlkjy.cn/api.php/v1.vod"},
+    {"name":"聚焦","url":"https://www.mjlnl.cn/api.php/v1.vod"},
+    {"name":"创意","url":"https://www.30dian.cn/api.php/v1.vod"},
+    {"name":"全民","url":"https://www.qmystv.com/api.php/v1.vod"}, 
+     {"name":"天天影视","url":"https://app.qianju.cc/api.php/v1.vod"},
+  {"name":"美剧饭","url":"https://ttzmz.net/api.php/v1.vod"},
+ {"name":"何以","url":"https://www.heyik.cn/api.php/v1.vod"},
+{"name":"聚焦","url":"https://www.mjlnl.cn/api.php/v1.vod"}
+     ];	
