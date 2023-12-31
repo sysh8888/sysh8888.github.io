fcb22768a8542f92211f553cbc2a76d2827364af
sysh8888 <sysh8888@126.com>
2023-12-31 09:08:58 +0800

    Update zy1.js

--- js/zy1.js
+++ js/zy1.js
@@ -3,7 +3,7 @@ var appzy=[
     {"name":"爱城","url":"https://90hd.top/api.php/v1.vod"}, 
     {"name":"奇乐","url":"https://qlkjy.cn/api.php/v1.vod"},
     {"name":"聚焦","url":"https://www.mjlnl.cn/api.php/v1.vod"},
-    {"name":"创意","url":"https://www.30dian.cn/api.php/v1.vod"},
+    {"name":"追剧","url":"https://vip.ystv.lol/api.php/v1.vod"},
     {"name":"全民","url":"https://www.qmystv.com/api.php/v1.vod"}, 
      {"name":"天天影视","url":"https://app.qianju.cc/api.php/v1.vod"},
   {"name":"美剧饭","url":"https://ttzmz.net/api.php/v1.vod"},
